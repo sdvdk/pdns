a8509f02b823d783b30928abb36380e601005662
sdvdk <steven@vanderkeur.com>
2014-12-06 22:14:30 +0100

    Change acceptance tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper_acceptance'
 
-describe 'pdns class' do
+describe 'pdns::nameserver' do
 
   context 'default parameters' do
+    let(:facts) {{
+      :osfamily => 'Debian'
+    }}
     # Using puppet_apply as a helper
     it 'should work idempotently with no errors' do
       pp = <<-EOS
@@ -14,11 +17,39 @@ describe 'pdns class' do
       apply_manifest(pp, :catch_changes  => true)
     end
 
-    describe package('pdns') do
+    describe package('pdns-server') do
       it { is_expected.to be_installed }
     end
 
     describe service('pdns') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
+end
+
+describe 'pdns::recursor' do
+
+  context 'default parameters' do
+    let(:facts) {{
+      :osfamily => 'Debian'
+    }}
+    # Using puppet_apply as a helper
+    it 'should work idempotently with no errors' do
+      pp = <<-EOS
+      class { 'pdns': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+
+    describe package('pdns-recursor') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('pdns-recursor') do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
