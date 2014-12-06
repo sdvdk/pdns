06028468f91803b5293c68ac5fd24856a3905f9e
sdvdk <steven@vanderkeur.com>
2014-12-06 14:27:26 +0100

    Add ability to install and configure namserver and/or recursor separately

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,22 @@
 #
 # Full description of class pdns here.
 #
-class pdns {
-  include pdns::nameserver
-  include pdns::recursor
+# === Parameters
+#
+# [nameserver]
+#   Default set to true. Installs and configures PowerDNS nameserver.
+#
+# [recursor]
+#   Default set to true. Installs and configures PowerDNS recursor.
+#
+class pdns (
+  $nameserver = true,
+  $recursor = true
+) {
+  if $nameserver {
+    include pdns::nameserver
+  }
+  if $recursor {
+    include pdns::recursor
+  }
 }
